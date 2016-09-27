394d59fc7b6ff70f26d6aa90f9b45aa46681c396
Gérald Meurant <gerald.meurant@lyra-network.com>
2016-09-27 11:49:50 +0200

    Changed command line parameters + Introduced quiet mode + Introduced verticalOffset for grabbing text + Added conversion of time values

--- src/main/groovy/fr/afjt/tools/PaycheckParser.groovy
+++ src/main/groovy/fr/afjt/tools/PaycheckParser.groovy
@@ -10,21 +10,55 @@ class PaycheckParser {
     static void main(String... args) {
         println "Paycheck Parser for A.F.J.T."
 
+        // Load the json config file
         def configFile = new File("./config.json")
         def jsonConfig = new JsonSlurper().parseText(configFile.text)
-        jsonConfig.data.each {
-            println it.name
-        }
 
-        def cli = new CliBuilder(usage: 'run.sh <pdfFiles>')
-        cli.paycheck(args: 1, argName: 'file', 'use given file for paycheck input')
-
+        // Parse the command line arguments
+        def cli = new CliBuilder(usage: 'run.sh <pdf files or folders>')
+        cli.verticalOffset(args: 1, argName: 'offset', 'adjust vertical coordinates for grabbing text')
+        cli.q('quiet mode: print only the short file name and the fields value separated by tabs')
+        cli.convertTime('convert <hour>h<minute>min time to fractional hours')
         def options = cli.parse(args)
 
-        if (options.paycheck) {
+        // Override quiet mode from command line
+        if (options.q) {
+            jsonConfig.quiet = new Boolean(options.q)
+        }
+
+        // Override verticalOffset from command line
+        if (options.verticalOffset) {
+            jsonConfig.verticalOffset = options.verticalOffset
+        }
+
+        // Override convertTime from command line
+        if (options.convertTime) {
+            jsonConfig.convertTime = options.convertTime
+        }
+
+        // Each argument represent either a file name or a folder name
+        if (options.arguments()) {
             def parser = new PaycheckParser()
-            println "Paycheck file : " + options.paycheck
-            parser.parse(options.paycheck, jsonConfig)
+            def files = new ArrayList<File>()
+            options.arguments().each {
+                files << new File(it)
+            }
+
+            files.each { f ->
+                if (f.isDirectory()) {
+                    println "-- ${f.name} --"
+
+                    f.eachFile {
+                        print "${it.name - '.pdf'} :"
+                        parser.parse(it.canonicalPath, jsonConfig)
+                    }
+                } else {
+                    println "Paycheck file : ${f}"
+                    parser.parse(f.canonicalPath, jsonConfig)
+                }
+            }
+        } else {
+            println cli.usage
         }
     }
 
@@ -33,14 +67,35 @@ class PaycheckParser {
 
         jsonConfig.data.each {
 
-            Rectangle rect = new Rectangle(it.location.x, it.location.y, it.location.width, it.location.height)
+            Rectangle rect = new Rectangle(it.location.x, it.location.y + jsonConfig.verticalOffset.toFloat(), it.location.width, it.location.height)
 
             RenderFilter[] filter = new RegionTextRenderFilter(rect);
             TextExtractionStrategy strategy = new FilteredTextRenderListener(new LocationTextExtractionStrategy(), filter);
-            println "Text parsed for '${it.name}':" + PdfTextExtractor.getTextFromPage(reader, it.location.page, strategy)
+            String value = PdfTextExtractor.getTextFromPage(reader, it.location.page, strategy)
+
+            // convert timestamp from format <hour>h<minute>min to fractional hours
+            if (it.type == "time" && jsonConfig.convertTime) {
+                // do the conversion
+                def pattern = /([^h]+)h([^m]+)min/
+                def group = (value =~ /$pattern/)
+                assert group.hasGroup()
+                assert 1 == group.size()
+
+                def hours = (group[0][1]).toFloat();
+                def minutes = (group[0][2]).toFloat();
+                def fractionalHours = hours + (minutes / 60.0)
+
+                value = sprintf('%.2f', fractionalHours)
+            }
+
+            if (jsonConfig.quiet) {
+                print value + '\t'
+            } else {
+                println "Text parsed for '${it.name}':" + value
+            }
 
         }
-
+        println ''
         if (jsonConfig.scan) {
             for (int y = 0; y < 600; y += 15) {
                 for (int x = 0; x < 800; x += 50) {
@@ -49,7 +104,7 @@ class PaycheckParser {
                     TextExtractionStrategy strategy = new FilteredTextRenderListener(new LocationTextExtractionStrategy(), filter);
                     String text = PdfTextExtractor.getTextFromPage(reader, 1, strategy)
                     if (text != '') {
-                        println "${x},${y}:"+text
+                        println "${x},${y}:" + text
                     }
                 }
             }
